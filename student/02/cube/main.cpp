190af9a207010d49d29d70bf87f4f22e498d99f0
Valtteri Kirsila <kirsila@linux-desktop10.cc.tut.fi>
2019-01-23 20:04:23 +0200

    uusi yritys

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -9,11 +9,12 @@ int main()
     cin >> n;
     if (n==0)
     {cout << "The cube of 0 is 0."<<endl;}
+    else {
     int n3 = 0;
     n3 = n*n*n;
     if (n3/n != n*n){
     cout << "Error! The cube of "<< n <<" is not "<< n3 <<"."<<endl;}
     else {
     cout << "The cube of " << n << " is " << n3 << "." << endl;
-    }
+    }}
 }
