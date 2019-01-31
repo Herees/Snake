9bd2b9ccc7fabea82dc59ef0b2fe50ed04537440
Valtteri Kirsila <kirsila@linux-desktop5.cc.tut.fi>
2019-01-31 20:18:58 +0200

    molkky koodit valmiina

---
+++ student/03/molkky/player.cpp
@@ -0,0 +1,28 @@
+#include "player.hh"
+#include <string>
+#include <iostream>
+Player::Player(std::string layer){
+    name_ = layer;
+}
+std::string Player::get_name(){
+return name_;
+}
+
+int Player::get_points(){
+return points_;
+}
+
+void Player::add_points(int a){
+    points_ += a;
+            if(points_ > 50){
+            points_ = 25;
+std::cout << name_ <<" gets penalty points!"  << std::endl;
+}
+}
+bool Player::has_won(){
+    if(Player::get_points()==50){
+        return true;
+    }
+        return false;
+    }
+
