cecf1168a24e1fe892d3cc46eb5213ffe790d9d0
Valtteri Kirsila <kirsila@linux-desktop5.cc.tut.fi>
2019-02-14 20:57:16 +0200

    line numbers ensimmäinen palautus

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input_nimi, output_nimi = "";
+    cout << "Input file: ";
+    getline(cin, input_nimi);
+    cout << "Output file: ";
+    getline(cin, output_nimi);
+
+    ifstream input(input_nimi);
+    if (! input) {
+        cout << "Error! The file " << input_nimi << " could not be opened." << endl;
+        return EXIT_FAILURE;
+    } else {
+        string row;
+        ofstream output(output_nimi);
+        for(unsigned int i = 1; getline(input, row); i++){
+            output << i << " " << row << endl;
+        }
+        output.close();
+        input.close();
+    }
+}
